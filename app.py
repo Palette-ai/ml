c32c939d171a8dd77552db88e71dd120ebfa4a8b
John Connor Quigley <john.c.quigley.21@dartmouth.edu>
2021-05-13 19:06:40 -0400

    testing port assignment

--- app.py
+++ app.py
@@ -26,6 +26,9 @@ app = Flask(__name__)
 glove = api.load("glove-wiki-gigaword-50")    
 similarity_index = WordEmbeddingSimilarityIndex(glove)
 
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
+
 
 def extract_average(json):
     try:
