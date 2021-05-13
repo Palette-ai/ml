e18fbfd128b4de81f1cff4ed7f664a5547ed0e75
John Connor Quigley <john.c.quigley.21@dartmouth.edu>
2021-05-13 19:56:32 -0400

    setting port

--- app.py
+++ app.py
@@ -26,8 +26,6 @@ app = Flask(__name__)
 glove = api.load("glove-wiki-gigaword-50")    
 similarity_index = WordEmbeddingSimilarityIndex(glove)
 
-port = int(os.environ.get("PORT", 5000))
-app.run(debug=True, host='0.0.0.0', port=port)
 
 
 def extract_average(json):
@@ -214,4 +212,7 @@ def index():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=7000)
+    # app.run(threaded=True, port=7000)
+
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
