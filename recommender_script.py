f0b3a308b2054964b654e35fbaf9729581e53334
John Connor Quigley <john.c.quigley.21@dartmouth.edu>
2021-05-25 17:21:49 -0400

    rename dish_df

--- recommender_script.py
+++ recommender_script.py
@@ -33,8 +33,8 @@ item_sim_model.save('s3://paletterecommendermodel/finalized_recommender_model/')
 dishes = db.dishes
 dish_cursor = dishes.find()
 list_cur_dish = list(dish_cursor)
-dish_f = pd.DataFrame(list_cur_dish)
-dish_f = dish_df.drop(['dish_id_num', 'dish_name','restaurant_id', 'features', 'description', 'tags', 'rating_ids', 'createdAt', 'price', 'img'], axis=1)
+dish_df = pd.DataFrame(list_cur_dish)
+dish_df = dish_df.drop(['dish_id_num', 'dish_name','restaurant_id', 'features', 'description', 'tags', 'rating_ids', 'createdAt', 'price', 'img'], axis=1)
 
 for row in dish_df.iterrows():
     dish_id = row[1]['_id']
